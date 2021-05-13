d55bff9f2bc7497fe8b895e1930107cdbb2a29c8
unknown <enthony10000@gmail.com>
2021-05-13 12:11:33 -0300

    Subindo arquivos

---
+++ teste.java
@@ -0,0 +1,14 @@
+/**
+ * teste
+ */
+public class teste {
+    public static void main(String[] args) {
+        if (1 == 2){
+            System.out.println(1+1);
+        }
+        else{
+            System.out.println(3);
+        }
+    }
+    
+}
